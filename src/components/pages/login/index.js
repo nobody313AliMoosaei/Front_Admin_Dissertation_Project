b538a84fc534110ea36a8bd7e4869b4cf48fb86a
miladzahed <defated.flower@gmail.com>
2023-05-22 02:11:31 +0330

    first change

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -5,13 +5,13 @@ const Login = () => {
     <div className="flex justify-center ">
       <div className="bg-[#fff] sm:p-10 px-5 py-8 rounded-lg w-[30rem] mx-8">
         <div className="flex justify-center">
-          <span className="sm:text-2xl text-base text-center font-bold">
-            ورود به سامانه ثبت پایان نامه
+          <span className="text-base font-bold text-center sm:text-2xl">
+            ورود به پنل ادمین
           </span>
         </div>
         <div className="mt-8">
           <div className="flex flex-col my-5">
-            <sapn className="sm:text-lg text-sm">نام کاربری</sapn>
+            <sapn className="text-sm sm:text-lg">نام کاربری</sapn>
             <input
               className="border-2 focus:ring focus:ring-[#003B7E] focus:outline-none focus:border-0 border-[#9B9B9B] rounded-md mt-1 sm:h-12 h-10 p-1 sm:text-lg text-sm "
               placeholder="  نام کاربری خود را وارد کنید"
@@ -19,7 +19,7 @@ const Login = () => {
             />
           </div>
           <div className="flex flex-col my-5">
-            <sapn className="md:text-lg text-sm">رمز عبور</sapn>
+            <sapn className="text-sm md:text-lg">رمز عبور</sapn>
             <input
               className="border-2 border-[#9B9B9B] focus:ring focus:ring-[#003B7E] focus:outline-none focus:border-0 rounded-md mt-1 sm:h-12 h-10 p-1 sm:text-lg text-sm "
               placeholder="رمزعبور خود را وارد کنید"
