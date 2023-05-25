f99a11f7e21ade01e305d84e075e83408d5d9a46
miladzahed <defated.flower@gmail.com>
2023-05-25 13:02:43 +0330

    create routes

---
+++ src/pages/dissertation/index.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+
+const Dissertation = () => {
+  return (
+    <div className="bg-[#F5F6FA] w-full pb-10 ">
+      <span className="text-black">dissertation</span>
+    </div>
+  );
+};
+
+export default Dissertation;
