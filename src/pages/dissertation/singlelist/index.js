c915c6519d1911ce589fc6cd3f1c606c4d90097e
alimahjub <alimahjub.138064m@gmail.com>
2023-08-08 12:30:11 +0430

    set scroll for table dissertation

--- src/pages/dissertation/singlelist/index.js
+++ src/pages/dissertation/singlelist/index.js
@@ -1,34 +1,38 @@
 import { Link } from "react-router-dom";
 
-const SingleList = ({ id, name, family, number, title ,download_dissertation,download_sourat,type}) => {
+const SingleList = ({
+  id,
+  name,
+  family,
+  number,
+  title,
+  download_dissertation,
+  download_sourat,
+  type,
+}) => {
   return (
-    <div className="grid grid-cols-8 p-3 rounded-sm ">
+    <div className="grid grid-cols-8 p-3 rounded-sm min-w-[900px]">
       <span className="col-span-1">{id}</span>
       <span className="">{name}</span>
       <span className=""> {family}</span>
       <span className="lg:col-span-1">{number}</span>
       <span className="col-span-1 truncate ">{title}</span>
-        <button className="text-[#4c5053] w-fit px-2 py-1 rounded-sm">
-         دانلود
-        </button>
-        <button className="text-[#4c5053] w-fit px-2 py-1 rounded-sm col-span-1">
-         دانلود
-        </button>
+      <button className="text-[#4c5053] w-fit px-2 py-1 rounded-sm">
+        دانلود
+      </button>
+      <button className="text-[#4c5053] w-fit px-2 py-1 rounded-sm col-span-1">
+        دانلود
+      </button>
       <Link to={``}>
-        
-          {type==="acc" ?
-          (
-            <button className="text-[#2080F6] bg-[#EBF1FD] w-fit justify-self-end px-2 py-1 rounded-sm col-span-1 sm:col-span-1">
-         تایید نهایی
-        </button>
-          ) :
-          (
-<button className="text-[#f62020] bg-[#fde2e2] w-fit justify-self-end px-2 py-1 rounded-sm col-span-1 sm:col-span-1">
-         رد ادمین
-        </button>
-          )
-}
-        
+        {type === "acc" ? (
+          <button className="text-[#2080F6] bg-[#EBF1FD] w-fit justify-self-end px-2 py-1 rounded-sm col-span-1 sm:col-span-1">
+            تایید نهایی
+          </button>
+        ) : (
+          <button className="text-[#f62020] bg-[#fde2e2] w-fit justify-self-end px-2 py-1 rounded-sm col-span-1 sm:col-span-1">
+            رد ادمین
+          </button>
+        )}
       </Link>
     </div>
   );
--- src/pages/dissertation/tablelist/index.js
+++ src/pages/dissertation/tablelist/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import SingleList from "../singlelist";
 import Pagination from "../../../components/common/pagination";
-const Dissertation = ({ check,typetab }) => {
+const Dissertation = ({ check, typetab }) => {
   const thesis = [
     {
       id: "1",
@@ -23,13 +23,12 @@ const Dissertation = ({ check,typetab }) => {
       family: "محجوب",
       number: "3981231094",
       title: "سامانه پایان‌نامه",
-    }
+    },
   ];
   return (
-    <div >
-           
-      <div className="bg-white min-h-[19rem] p-4 my-5 rounded-md">
-        <div className="grid grid-cols-8 font-medium bg-[#F8F9FA]  p-3 rounded-sm ">
+    <div className="w-full">
+      <div className="bg-white min-h-[19rem] p-4 my-5 rounded-md  overflow-x-auto ">
+        <div className="grid grid-cols-8  font-medium bg-[#F8F9FA] min-w-[900px]  p-3 rounded-sm ">
           <span className="col-span-1">#</span>
           <span className="">نام</span>
           <span className="">نام خانوادگی</span>
@@ -38,7 +37,8 @@ const Dissertation = ({ check,typetab }) => {
           <span className="">پایان نامه </span>
           <span className="">صورت جلسه </span>
         </div>
-        {thesis.length ?(Array.isArray(thesis) &&
+        {thesis.length ? (
+          Array.isArray(thesis) &&
           thesis.map((item, index) => (
             <SingleList
               check={check}
@@ -51,20 +51,17 @@ const Dissertation = ({ check,typetab }) => {
               type={typetab}
             />
           ))
-          ):
-          (
-            <div className="m-10 justify-items-center">
+        ) : (
+          <div className="m-10 justify-items-center">
             <span className="self-center p-5 my-5 bg-orange-300 rounded-md w-fit">
-        متاسفانه هیچ داده ای یافت نشد!
-      </span>
-      </div>
-          )
-        }
+              متاسفانه هیچ داده ای یافت نشد!
+            </span>
+          </div>
+        )}
         {/* <Pagination count={thesis.length} /> */}
       </div>
     </div>
-    
-    );
+  );
 };
 
 export default Dissertation;
